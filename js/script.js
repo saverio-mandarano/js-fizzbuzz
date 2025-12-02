991ab67424a640d38615875110f46f875f0b3d30
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-02 15:48:15 +0100

    provo a modificare le condizioni per aggiungere Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 // Consegna:
 // Scrivi un programma che stampi i numeri da 1 a 100,
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 for (let i=1; i<=100; i++){
-
-    if ( i % 3 === 0) {
+// ma per i multipli di 3 stampi “Fizz” al posto del numero
+    if ( i % 3 === 0 && i % 5 !==0) {
         console.log("Fizz");
     }
-    else if ( i % 5 === 0){
+// e per i multipli di 5 stampi Buzz.
+    else if ( i % 5 === 0 && i % 3 !==0){
         console.log("Buzz");
+    }
+    else if ( i % 3 === 0 && i % 5 ===0){
+        console.log("FizzBuzz");
     }
     else{
         console.log(i);
     }
 }
-// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 // Prima di partire a scrivere codice poniamoci qualche domanda:
 // Come faccio a sapere se un numero è divisibile per?
