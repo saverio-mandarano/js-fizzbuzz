4635f76a6083b9d6116837953404ca6ec1cc5eb0
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-02 15:33:50 +0100

    inizio

--- js/script.js
+++ js/script.js
@@ -1,31 +1,12 @@
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-const km= parseFloat(prompt("Inserisci il numero di chilometri che vuoi percorrere:"))
-const età= parseInt(prompt("Adesso inserisci l'età del passeggero:"))
+// Consegna:
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// Il prezzo del biglietto è definito in base ai km (0.21 € al km)
-const prezzoIniziale= km * 0.21;
-let sconto= 0;
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per?
+// Abbiamo visto qualcosa di particolare che possiamo usare?
 
-// va applicato uno sconto del 20% per i minorenni
-if (età < 18){
-    sconto = 0.20;
-} 
-// va applicato uno sconto del 40% per gli over 65.
-else if (età > 65) {
-    sconto = 0.40;
-} 
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
-// Output partite da semplice log, e poi se volete stampate in pagina come sapete fare.
-const prezzoFinale = prezzoIniziale * (1 - sconto);
-console.log(`Il prezzo del biglietto è di: € ${prezzoFinale}` );
-// alert(`Il prezzo del biglietto è di: € ${prezzoFinale}` );
-document.getElementById("prezzo-biglietto-iniziale").innerHTML = "Il prezzo del biglietto iniziale è di: € " + prezzoIniziale;
-document.getElementById("prezzo-biglietto-finale").innerHTML += ` € ${prezzoFinale}`
-document.getElementById("sconto").innerHTML += ` ${sconto * 100} %`;
-
-
-// TIP:
-// per controllare che la vostra logica sui prezzi funzioni correttamente, provate a verificare quanto segue:
-// 100km, 10 anni => prezzo corretto:  €16.80
-// 100km, 70 anni => prezzo corretto: €12.60
+// Consigli del giorno:
+// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
