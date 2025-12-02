6c4db5de566330dda985a395192e14fee75670b1
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-02 15:40:40 +0100

    add if ed else if

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // Consegna:
 // Scrivi un programma che stampi i numeri da 1 a 100,
+for (let i=1; i<=100; i++){
+
+    if ( i===3) {
+        console.log("Fizz");
+    }
+    else if ( i===5){
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
